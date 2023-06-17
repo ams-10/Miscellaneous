c681f0a353410ebaa9cebf6ed683be1b8d8fdcb5
ams-10 <abdullahshareefh@gmail.com>
2023-06-17 11:03:07 -0300

    adding completed

--- Mad.py
+++ Mad.py
@@ -1,18 +1,21 @@
-# name1 = input("Enter first name : ")
-# name2 = input("Enter second name : ")
+name1 = input("Enter first name : ")
+name2 = input("Enter second name : ")
 
-# var = name1 + name2
-# var2 = var.replace(" " , "")
-# print(var2)
+var = name1 + name2
+var2 = var.replace(" " , "")
+print(var2)
 
 exstr = "123321"
 ls = []
+
 for i in range(0,len(exstr)):
     ls.append(exstr[i])
-print(ls)
+print("\n",ls,"\n")
+
 ls = [int(x) for x in ls]
-print(ls)
+print(ls,"\n")
 l = len(ls)
+
 sum = 0
 for num in range(l):
     if num % 2 == 0 :
